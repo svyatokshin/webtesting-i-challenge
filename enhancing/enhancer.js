0cf189d8e0cc94173981a4c5dd63d5b24ad6dbe8
svyatoslav-okshin <svyatokshin@gmail.com>
2020-03-25 16:56:13 -0400

    finished a few tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,51 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (isNumber(item.enhancement)) {
+   if (item.enhancement < 20) {
+    item.enhancement += 1;
+    return { ...item };
+  } else {
+    throw new Error("You cannot gain enhancement level if you are enhancement level 20")
+  }
+  } else {
+    throw new Error("Incorrect enhancement value");
+  }
+  
+  
 }
 
 function fail(item) {
-  return { ...item };
+  if (isNumber(item.enhancement)) {
+    if (item.enhancement < 15) {
+     item.durability -= 5;
+     return { ...item };
+     
+   } else if (item.enhancement >= 15) {
+     item.durability -= 10;
+     if (item.enhancement > 16) {
+       item.enhancement -= 1;
+       return { ...item };
+     } else {
+       return { ...item };
+     }}
+     
+   } else {
+     throw new Error("Incorrect enhancement value");
+   }
+   
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
 function get(item) {
   return { ...item };
+}
+
+
+function isNumber(value) {
+  return !isNaN(parseFloat(value)) && isFinite(value);
 }
